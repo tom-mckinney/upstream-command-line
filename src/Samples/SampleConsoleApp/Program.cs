934451d0602ec1925f9ea250ce2e11da6955f7ba
Tom McKinney <tom@upstreamcode.com>
2020-01-05 14:36:54 -0500

    Add workflows and core classes
    - CommandLineApplication
    - CommandBase
    - CommandLineException

---
+++ src/Samples/SampleConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using SampleConsoleApp.Commands;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Upstream.CommandLine;
+
+namespace SampleConsoleApp
+{
+    public class Program
+    {
+        public static Stopwatch Stopwatch { get; } = new Stopwatch();
+
+        public static Task Main(string[] args)
+        {
+            Stopwatch.Start();
+
+            return new CommandLineApplication()
+                .AddCommand<FooCommand>()
+                .InvokeAsync(args);
+        }
+    }
+}
---
+++ src/Upstream.CommandLine/CommandLineException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Upstream.CommandLine
+{
+    public class CommandLineException : Exception
+    {
+        public CommandLineException(string message) : base(message)
+        {
+        }
+
+        public CommandLineException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
