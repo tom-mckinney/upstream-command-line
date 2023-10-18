99cc8c2095370221e6a60b14e04b2bbe2250ec37
Tom McKinney <tom@upstream.dev>
2023-10-17 23:53:58 -0400

    Trying to get augmentation to work

--- src/Upstream.CommandLine.Generator/UserClass.cs
+++ src/Upstream.CommandLine.Generator/UserClass.cs
@@ -1,10 +1,25 @@
-using GeneratedNamespace;
-
 public partial class UserClass
 {
     public void UserMethod()
     {
-        // call into a generated method
+        // call into a generated method inside the class
+        Console.WriteLine("WHAT?!");
+        this.GeneratedMethod();
+        Console.WriteLine("WHAT?!");
+        
+        Console.WriteLine($"Does this work?: {this.Test()}");
+        
         GeneratedNamespace.GeneratedClass.GeneratedMethod();
     }
 }
+
+// using GeneratedNamespace;
+//
+// public partial class UserClass
+// {
+//     public void UserMethod()
+//     {
+//         // call into a generated method
+//         GeneratedNamespace.GeneratedClass.GeneratedMethod();
+//     }
+// }
